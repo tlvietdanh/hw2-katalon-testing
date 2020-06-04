aa4d03971bfcb639fd3dd878758b4584d8b6d308
Tran Quoc Cuong <cuong934073@gmail.com>
2020-06-04 19:10:39 +0700

    test case login 01

---
+++ Test Suites/TS_Login/TS_login_01.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_login_01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0fe3a9ee-86ca-4ebe-a2e3-ed0207a9268d</testSuiteGuid>
+   <testCaseLink>
+      <guid>ab3f775e-205f-4bac-9b07-fabe0aa08149</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC_login_01</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>23bf43ca-373e-4239-bb8c-fad3f037f7f3</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Login_data/login_data_01</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>23bf43ca-373e-4239-bb8c-fad3f037f7f3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>user</value>
+         <variableId>d52eb655-b3dd-4000-9520-44aa81e8127f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>23bf43ca-373e-4239-bb8c-fad3f037f7f3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>cab29259-a1fc-4212-9894-53fa8b8abde8</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
