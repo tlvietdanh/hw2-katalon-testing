d80f619f5afea89dd645e822de266ade0ad5f7bc
tlvietdanh <tlvietdanh@gmail.com>
2020-06-05 15:47:15 +0700

    add test suilt

---
+++ Object Repository/Rename_channel/Page_test_name_4 - test Mattermost/span_Mute Channel.rs
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Mute Channel</name>
+   <tag></tag>
+   <elementGuidId>47f8b996-541b-47a5-a5ad-f2d623bdd386</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>MenuItem__primary-text</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Mute Channel</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;channelToggleMuteChannel&quot;)/button[@class=&quot;style--none&quot;]/span[@class=&quot;MenuItem__primary-text&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//li[@id='channelToggleMuteChannel']/button/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Notification Preferences'])[1]/following::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='View Info'])[1]/following::span[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Add Members'])[1]/preceding::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Manage Members'])[1]/preceding::span[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Mute Channel']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[4]/button/span</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Permission/Change_Permission.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Change_Permission</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e6e85cb5-ffa4-41b6-bf00-30b3a3b9531b</testSuiteGuid>
+   <testCaseLink>
+      <guid>f5753295-289f-48a1-ac0f-2e8055607777</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SetPermissionForUser/Change_member_role</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>6ac3bbd2-fa65-4db7-8b8e-6cc4a2aea8d8</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Change_Permission/Change_permission_member</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>6ac3bbd2-fa65-4db7-8b8e-6cc4a2aea8d8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>target_email</value>
+         <variableId>41ab9075-a3ad-4411-af10-6100f8e53a9d</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>982290f8-f7b3-48cb-8251-b3efe146eddf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SetPermissionForUser/Change_admin_role</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>c4e7b423-30d8-4854-9f08-56ea46ff0a28</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Change_Permission/Change_permission_admin</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>c4e7b423-30d8-4854-9f08-56ea46ff0a28</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>target_email</value>
+         <variableId>e4ff4416-429c-4a2e-b80d-e29144c498d7</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
